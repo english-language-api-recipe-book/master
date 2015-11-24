6d78c5d5e6acb8a41539d60dd04ca2fca3bddea1
Kin Lane <info@apievangelist.com>
2015-11-23 17:32:03 -0800

    debug

--- js/navigator-apis-json.js
+++ js/navigator-apis-json.js
@@ -244,10 +244,10 @@ function APIJSONNavigatorGetIncludeListing($includeName,$includeRootUrl,$include
   html = html + '<td align="left" style="padding-left: 150px; padding-top: 5px; padding-bottom: 5px;" id="include-cell-' + $includecount + '">';
   html = html + '<a href="' + $includeUrl + '" style="color: #000; font-size: 24px; text-decoration: none;" title="' + $includeName + '"><strong>' + $includeName + '</strong></a>';
   html = html + '</td>';
-  html = html + '<td align="center" style="padding-left: 150px; padding-top: 5px; padding-bottom: 5px;">';
+  html = html + '<td align="center" style="padding-top: 5px; padding-bottom: 5px;">';
   html = html + '(<a href="' + $includeUrl + '" style="color: #000; font-size: 18px; text-decoration: none;" title="' + $includeName + '">site</a>)';
   html = html + '</td>';
-  html = html + '<td align="center" style="padding-left: 150px; padding-top: 5px; padding-bottom: 5px;">';
+  html = html + '<td align="center" style="padding-top: 5px; padding-bottom: 5px;">';
   html = html + '(<a href="' + $includeRootUrl + '" style="color: #000; font-size: 18px; text-decoration: none;" title="' + $includeName + '">apis.json</a>)';
   html = html + '</td>';
 
